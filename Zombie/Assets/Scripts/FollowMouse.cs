b216a4826748efdc73ae0aba1a7a301492bb6809
Frankiplier <agatarucka@gmail.com>
2025-04-06 14:00:57 +0200

    Unsmoothed the camera

--- Zombie/Assets/Scripts/FollowMouse.cs
+++ Zombie/Assets/Scripts/FollowMouse.cs
@@ -2,17 +2,14 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
-    public float followSpeed;
-    private Vector3 targetPosition;
-
     void Update()
     {
         Vector3 mousePos = Input.mousePosition;
 
-        targetPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
         
-        targetPosition.z = transform.position.z;
+        worldMousePos.z = transform.position.z;
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        transform.position = worldMousePos;
     }
 }
